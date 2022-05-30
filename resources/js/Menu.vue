19ff1228cd23d884cb507ecdc08f3ea885606271
rouamansour <rouamansour.rm@gmail.com>
2022-05-30 21:10:03 +0100

    update front end : vue js

--- resources/js/Menu.vue
+++ resources/js/Menu.vue
@@ -1,32 +1,59 @@
 <template>
-    
-    <nav class="navbar navbar-dark bg-dark justify-content-between flex-nowrap flex-row">
-        
-        <div class="container">
-            
-            <a class="navbar-brand float-left">
-                <router-link class="nav-link navbar-brand pr-3" to="/register">Authentification</router-link>
-            </a>
-            <ul class="nav navbar-nav flex-row float-right">
-                <li class="nav-item">
-                    <router-link class="nav-link navbar-brand pr-3" to="/"> Home </router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+         <!-- <i class="fa fa-home"></i> -->
+        <i class="fa fa-home" aria-hidden="true"></i>
+        <a class="navbar-brand" href="/">Acceuil</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <router-link
+                        class="nav-link navbar-brand pr-3"
+                        to="/participants"
+                    >
+                        Listes Participants
+                    </router-link>
                 </li>
-                    <li class="nav-item">
-                    <router-link class="nav-link navbar-brand pr-3" to="/participants"> Participants </router-link>
+                <li class="nav-item active">
+                    <router-link
+                        class="nav-link navbar-brand pr-3"
+                        to="/addParticipant"
+                    >
+                        Ajout Participant
+                    </router-link>
                 </li>
-                <li class="nav-item">
-                    <router-link class="nav-link navbar-brand pr-3" to="/addParticipant">Ajout Participant </router-link>
-                </li>
-                <li>
-                    <a class="navbar-brand float-left">
-                            <router-link class="nav-link" to="/logout"> Logout </router-link>
-                    </a>
-                </li>
+
+                    <!-- <li class="nav-item active">
+                        <router-link
+                            class="form-control mr-sm-2"
+                            to="/register"
+                        >
+                            Register
+                        </router-link>
+                    </li>
+                    
+                    <li class="nav-item active">
+                        <router-link class="form-control mr-sm-2" to="/logout">
+                            Logout
+                        </router-link>
+                    </li> -->
+
             </ul>
         </div>
     </nav>
 </template>
 
 <script>
-    export default {};
+export default {};
 </script>
